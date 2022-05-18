a23852fb92fbbac102f151a444e9117ad059a907
SimplyElvis <provictor.ie@gmail.com>
2022-05-18 12:39:11 +0100

    svelte page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script>
-	export let name;
+	import FeedbackList from './components/FeedbackList.svelte';
+	import FeedbackStats from './components/FeedbackStats.svelte';
+	import FeedbackForm from './components/FeedbackForm.svelte';
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		}
+	];
+
+	$: count = feedback.length;
+	$: average = feedback.reduce((prev, {rating})=> {return prev + rating}, 0) / feedback.length;
+
+	const deleteFeedback = (e)=>{
+		const itemId = e.detail;
+		feedback = feedback.filter((item)=>{ return item.id != itemId});
+		console.log(feedback);
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+	<FeedbackForm/>
+	<FeedbackStats {count} {average}/>
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
